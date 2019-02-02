a676f0eea95636cdecc474625bf26d47666b385e
Nate Cottle <cottle.nate@gmail.com>
2019-02-02 12:14:37 -0800

    Start adding components and containers for project

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux'
+
+
+class BurgerBulider extends Component {
+  render() {
+    return(
+      <Aux>
+        <div>Burger Built</div>
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBulider;
