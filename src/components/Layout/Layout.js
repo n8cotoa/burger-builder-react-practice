1f93707d14aa2996eb19cd4bde38ca38794a1df7
Nate Cottle <cottle.nate@gmail.com>
2019-02-02 19:37:46 -0800

    change layout to a stateful component, add functionality to hide sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Navbar from '../Navigation/Navbar/Navbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-  <Aux>
-    <Navbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: true})
+  }
+ 
+  render() {
+    return (
+      <Aux>
+        <Navbar />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+
+}
+
+export default Layout;
