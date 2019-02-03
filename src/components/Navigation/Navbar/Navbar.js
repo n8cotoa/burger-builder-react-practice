773eb1c47bf2b33858858d6940472c699e2b6236
Nate Cottle <cottle.nate@gmail.com>
2019-02-02 19:25:49 -0800

    Add media queries for displaying correct navigation

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -9,7 +9,7 @@ const navbar = ( props ) => (
     <div className={classes.Logo}>
       <Logo />
     </div>
-    <nav>
+    <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
